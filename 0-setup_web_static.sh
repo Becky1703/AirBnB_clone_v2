ccf61250028a3ae8e550ee4d218b7f825bfa7508
Becky1703 <oluwabukunmia@gmail.com>
2023-07-06 11:27:07 -0700

    versions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-print %s "
+printf %s "
 <html>
   <head>
   </head>
